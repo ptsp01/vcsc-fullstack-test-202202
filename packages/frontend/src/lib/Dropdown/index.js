ffe401a0838578d7e582a6bd5fee38a66ad56995
trungvt <thanhtrungvo8401@gmail.com>
2022-02-19 22:42:51 +0700

    FE-01-handle portal

--- packages/frontend/src/lib/Dropdown/index.js
+++ packages/frontend/src/lib/Dropdown/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import ReactDom from "react-dom";
 import DHeader from "./DHeader";
 import DItem from "./DItem";
-import { EMPTY } from "../../constant";
+import { EMPTY, PORTAL } from "../../constant";
 import { DContext } from "./DContext"
 
 import "./style.scss"
 
-
-
-function Dropdown ({ header, children, onSelect, label, isFluid }) {
+function Dropdown ({ header, children, onSelect, isFluid }) {
   const [isExpand, setExpand] = React.useState(false);
+  const [coordinate, setCoordinate] = React.useState({ top: 0, left: 0, width: 0 })
+  const portalEl = document.querySelector(`#${PORTAL}`);
+  const renderEl = React.useRef(document.createElement(`div`))
+
   const dRef = React.useRef();
 
   const toggleExpand = (val) => {
@@ -21,25 +24,38 @@ function Dropdown ({ header, children, onSelect, label, isFluid }) {
   }
 
   const handleClickOutside = (e) => {
-    const isOutside = !dRef.current?.contains(e.target);
+    const isOutside = !dRef.current?.contains(e.target) 
+      && !renderEl.current?.contains(e.target);
     if (isOutside) {
       toggleExpand(0);
     }
   }
 
+  const dropDownContent = <ul style={{...coordinate}} className={`content ${isExpand ? 'expand': EMPTY}`} >
+    { children }
+  </ul>;
+
   React.useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
+    portalEl.appendChild(renderEl.current)
+    renderEl.current.classList.add("dropdown")
+    const _renderEl = renderEl.current;
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      portalEl.removeChild(_renderEl)
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  React.useEffect(() => {
+    const _cor = dRef.current.getBoundingClientRect();
+    setCoordinate({ top: _cor.bottom, left: _cor.left, width: _cor.width })
+  }, [header])
 
   return<DContext.Provider value={{ onSelect }}>
       <div ref={dRef} className={`dropdown ${isFluid ? 'fluid' : EMPTY}`} onClick={toggleExpand} >
         { header }
-
-        <ul className={`content ${isExpand ? 'expand': EMPTY}`} >
-          { children }
-        </ul>
+        { ReactDom.createPortal(dropDownContent, renderEl.current) }
       </div>
     </DContext.Provider>
 }
