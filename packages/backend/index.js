08607f0f7bc2b4df5ebe0c126829ac2c38272203
Kyle Nguyen <kyle.nguyen304@gmail.com>
2022-05-21 12:15:10 -0400

    initial commit

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,6 +1,97 @@
-function decodeMsg() {
-    // Your code here
-    
+// const { DELIMITER } = require('path');
+const { mainModule } = require("process");
+
+const HEADER_TAG = "8=";
+const BODY_TAG = "9=";
+const DELIMITER = "";
+const EQUAL = "=";
+
+function cnvDataPair(data) {
+  let result = {};
+  if (data && data.includes(DELIMITER)) {
+    let pairs = data.split(DELIMITER);
+    pairs.forEach((pair) => {
+      if (pair.includes(EQUAL)) {
+        let data = pair.split(EQUAL);
+        let key = data[0];
+        let value = data[1];
+        result[key] = value;
+      }
+    });
+  }
+  return result;
 }
 
-module.exports = decodeMsg
+function filterNoise(data) {
+  if (data && data.includes(HEADER_TAG) && data.includes(BODY_TAG)) {
+    let HIndex = data.indexOf(HEADER_TAG);
+    let BIndex = data.indexOf(BODY_TAG);
+    let nextHIndex = data
+      .substring(HIndex + HEADER_TAG.length)
+      .indexOf(HEADER_TAG);
+    if (nextHIndex && nextHIndex <= BIndex) {
+      data = data.substring(nextHIndex);
+    }
+  }
+  return data;
+}
+
+function detachHeader(data) {
+  let headerIndex = data.indexOf(HEADER_TAG);
+  let bodyIndex = data.indexOf(BODY_TAG);
+  let headerStr = data.substring(headerIndex, bodyIndex);
+  let headerValue = headerStr
+    .substring(HEADER_TAG.length, bodyIndex)
+    .split(DELIMITER)[0];
+  return {
+    headerValue: headerValue,
+  };
+}
+
+function detachBody(data) {
+  let bodyRawValue = "";
+  let bodyLength = 0;
+  if (data && data.includes(BODY_TAG)) {
+    let bodyIndex = data.indexOf(BODY_TAG);
+    bodyLength = parseInt(
+      data.substring(bodyIndex + BODY_TAG.length).split(DELIMITER)[0]
+    );
+    let bodyStr = data.substring(bodyIndex);
+    let bodyBeginIndex = bodyStr.indexOf(DELIMITER);
+    let bodyEndIndex = bodyBeginIndex + bodyLength;
+    bodyRawValue = bodyStr.substring(bodyBeginIndex, bodyEndIndex);
+  }
+  return {
+    bodyLength: bodyLength,
+    bodyRawValue: bodyRawValue,
+  };
+}
+
+function decodeMsg() {
+  const fs = require("fs");
+  const raw = fs.readFileSync("./packages/backend/data.tcp", "utf-8");
+  let maniStr = raw;
+  let filteredData = [];
+  while (maniStr.includes(HEADER_TAG) && maniStr.includes(BODY_TAG)) {
+    maniStr = filterNoise(maniStr);
+    let headerData = detachHeader(maniStr);
+    let bodyData = detachBody(maniStr);
+    let bodyValue = cnvDataPair(bodyData.bodyRawValue);
+    let metaData = {};
+    metaData[HEADER_TAG.replace("=", "")] = headerData.headerValue;
+    metaData[BODY_TAG.replace("=", "")] = bodyData.bodyLength.toString();
+    let data = Object.assign(metaData, bodyValue);
+    filteredData.push(data);
+    let msgLength =
+      HEADER_TAG.length +
+      headerData.headerValue.length +
+      BODY_TAG.length +
+      bodyData.bodyLength.toString().length +
+      bodyData.bodyRawValue.length +
+      DELIMITER.length;
+    maniStr = maniStr.substring(msgLength);
+  }
+  return filteredData;
+}
+
+module.exports = decodeMsg;
---
+++ packages/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import { DropDown } from "./components/newDropdown.js";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <DropDown />
+    </div>
+  );
+}
+
+export default App;
